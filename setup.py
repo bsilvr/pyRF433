8fbb21216a3aadc110607594d2a61d9e87be6c0b
Bruno Silva <brunomiguelsilva@ua.pt>
2017-02-24 17:37:26 +0000

    updated to include all packages

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 
-from distutils.core import setup
+from distutils.core import setup, find_packages
 
 setup(
     name='RF433',
-    packages=['RF433'],
-    version='0.1.1',
+    packages=find_packages(),
+    version='0.1.2',
     description='RF433 library to send signals for various protocols using \
                     an arduino',
     author='Bruno Silva',
     author_email='brunomiguelsilva@ua.pt',
     url='https://github.com/bsilvr/pyRF433',
-    download_url='https://github.com/bsilvr/pyRF433/tarball/0.1.1',
+    download_url='https://github.com/bsilvr/pyRF433/tarball/0.1.2',
     keywords=['rf433', 'arduino'],
     classifiers=[],
 )
